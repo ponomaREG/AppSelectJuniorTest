9bb13a52474fbfcabf971425462e200fc652b6fe
ponomaREG <ponomarcomru@gmail.com>
2021-06-26 16:03:54 +0300

    Main

--- app/src/main/java/com/appselect/junior/ui/main/MainActivity.kt
+++ app/src/main/java/com/appselect/junior/ui/main/MainActivity.kt
@@ -3,15 +3,35 @@ package com.appselect.junior.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.ItemTouchHelper
 import com.appselect.junior.R
+import com.appselect.junior.databinding.ActivityMainBinding
+import com.appselect.junior.ui.adapter.MovieAdapter
+import com.appselect.junior.ui.util.SwipeTouchHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val movieAdapter = MovieAdapter()
+        val swipeTouchHelperCallBack = SwipeTouchHelper()
+        val itemTouchHelper = ItemTouchHelper(swipeTouchHelperCallBack)
+        itemTouchHelper.attachToRecyclerView(binding.rv)
+        binding.apply {
+            lifecycleOwner = this@MainActivity
+            vm = viewModel
+            adapter = movieAdapter
+            onSwipeRefresh.setOnRefreshListener {
+                movieAdapter.clearAll()
+                viewModel.loadMovies(0)
+            }
+        }
+        viewModel.loadMovies(0)
     }
 }
--- app/src/main/java/com/appselect/junior/ui/main/MainViewModel.kt
+++ app/src/main/java/com/appselect/junior/ui/main/MainViewModel.kt
@@ -1,13 +1,16 @@
 package com.appselect.junior.ui.main
 
+import android.util.Log
 import androidx.databinding.ObservableBoolean
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.appselect.junior.model.Movie
 import com.appselect.junior.repository.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: MainRepository
 ): ViewModel() {
@@ -17,6 +20,7 @@ class MainViewModel @Inject constructor(
     val toast: LiveData<String> = _toast
     val movies: LiveData<List<Movie>> = _movies
     val isLoading: ObservableBoolean = ObservableBoolean(false)
+
 
     fun loadMovies(page: Int){
         isLoading.set(true)
