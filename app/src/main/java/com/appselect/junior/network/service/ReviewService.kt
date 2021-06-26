072453a2672fe4c7db4bf565d4e5fff1fdc9fcfa
ponomaREG <ponomarcomru@gmail.com>
2021-06-26 16:03:02 +0300

    Add offset to ReviewService.kt

--- app/src/main/java/com/appselect/junior/network/service/ReviewService.kt
+++ app/src/main/java/com/appselect/junior/network/service/ReviewService.kt
@@ -1,5 +1,7 @@
 package com.appselect.junior.network.service
 
+import com.appselect.junior.model.Response
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -7,7 +9,7 @@ import retrofit2.http.Query
 interface ReviewService {
     @GET("/svc/movies/v2/reviews/all.json")
     fun getMovies(
-        @Query("page") page: Int,
+        @Query("offset") offset: Int,
         @Query("api-key") apiKey: String = "FbwuufLPDRjAlGUIQNAbrjuDxq3Why2a"
-    )
+    ): Observable<Response>
 }
