04ca892c1a6fe6fe4c1fba61b50be901fc02f250
ponomaREG <ponomarcomru@gmail.com>
2021-06-26 16:02:20 +0300

    Implements RxCallAdapter

--- app/src/main/java/com/appselect/junior/di/NetworkModule.kt
+++ app/src/main/java/com/appselect/junior/di/NetworkModule.kt
@@ -9,6 +9,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
@@ -30,6 +31,7 @@ object NetworkModule {
         return Retrofit.Builder()
             .baseUrl("https://api.nytimes.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(okHttpClient)
             .build()
     }
